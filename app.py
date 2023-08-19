a30fa239556bffd8ac901e27c22defffd5c17713
ajaykrishnan-rmit <110024295+ajaykrishnan-rmit@users.noreply.github.com>
2023-08-19 15:11:20 +1000

    Csv dump feature

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ import numpy as np
 from bs4 import BeautifulSoup
 from PIL import Image
 import requests
+import numpy 
 from transformers import BlipProcessor, BlipForConditionalGeneration
 ImageList=[]
+import csv
 
 
-df=pd.DataFrame(columns={'imageLink','Description','MetadataDump'})
+
+# loaded_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+# loaded_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16) 
@@ -27,6 +33,8 @@ def CaptionView():
 
 @app.route('/CaptionLink', methods=['GET', 'POST'])
 def CaptionLink():
+    global loaded_processor
+    global loaded_model
     if request.method == 'POST':
         imageLink = request.form['imageLink']
         tempDict={'ImageLink':imageLink,'Description':''}
@@ -40,10 +48,12 @@ def CaptionLink():
 
         image2 = Image.open(requests.get(imageLink, stream=True).raw).convert('RGB')
 
-        inputs = loaded_processor(image2, return_tensors="pt",padding=True)
+        inputs = loaded_processor(image2, return_tensors="pt")
 
         out = loaded_model.generate(**inputs)
-        tempDict['Description']=loaded_processor.decode(out[0], skip_special_tokens=True)
+        description=loaded_processor.decode(out[0], skip_special_tokens=True)
+        
+        tempDict['Description']=description.replace("arafed","")
 
         ImageList.append(tempDict)
     return render_template('Caption.html',ImageList=ImageList)
@@ -59,16 +69,27 @@ def remove_item(index):
 @app.route('/SaveCaption/<int:index>', methods=['GET', 'POST'])
 def SaveCaption(index):
     if request.method == 'POST':
+      
         ImageLink = request.form['ImageLink']
         Description = request.form['Description']
         MetadData={}
-        global df
-        df = df.append({'imageLink':ImageLink,'Description':Description,'MetadataDump':MetadData}, ignore_index=True)
+        
+        temp={'imageLink':ImageLink,'Description':Description,'MetadataDump':MetadData}
+
+        
+        with open('CaptionDump.csv', mode='a', newline='') as csv_file:
+            # Create DictWriter object
+            field_names=['imageLink','Description','MetadataDump']
+            writer = csv.DictWriter(csv_file, fieldnames=field_names)
+            
+            # Write the dictionary to CSV
+            writer.writerow(temp)
+
+
         if 0 <= index < len(ImageList):
             del ImageList[index]
-        df.to_csv("CaptionDump.csv")
-        pass
     return redirect('/CaptionLink')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
